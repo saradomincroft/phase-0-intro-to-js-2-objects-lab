5c2eee227352388d4c69a81d6383bc7ceaa988b6
Sara Croft <86280277+saradomincroft@users.noreply.github.com>
2024-02-06 11:23:24 +1100

    lab completed

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Write your solution in this file!
+let employee = {
+    name: "Sara Catalano",
+    streetAddress: "Power Street",
+
+}
+
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+    return {
+        ...employee,
+        [key]: value
+      };
+}
+
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function  deleteFromEmployeeByKey(employee, key) {
+    let updatedEmployee = { ...employee };
+    delete updatedEmployee[key];
+    return updatedEmployee;
+}
+
+function destructivelyDeleteFromEmployeeByKey(employee, key) {
+    delete employee[key];
+    return employee;
+}
